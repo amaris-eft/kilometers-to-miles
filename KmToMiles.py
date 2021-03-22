911b4817fca200630c80bc4826dd53d204499a2d
Amaris Efthimiou <51919178+amaris-eft@users.noreply.github.com>
2021-03-22 01:32:46 -0400

    Add files via upload

---
+++ KmToMiles.py
@@ -0,0 +1,13 @@
+#Name:Amaris Efthimiou
+#Email:amarisefthimiou@gmail.com
+#Date: September 2019
+#Converter km to miles
+
+
+print("Welcome to Amaris' calculator!")
+#1. Input the distance in kilometers (call it km)
+km = float(input('Enter distance in kilometers: '))
+#2. Calculate the distance in miles as 0.621371*km
+miles = 0.621371*km
+#3. Output miles
+print("The miles are", miles)
